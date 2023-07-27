8d04825781f39892a9d95c8211654bf904eea378
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-07-27 14:20:34 -0500

    lecture notes taken

---
+++ js/external_js.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log("this is an external js, you load me in with a src attribute")
+
+//user interaction
+
+//Alert; Confirm; Prompts;
+
+
+//Alerts spawns a small browser window with only one "accept" button
+
+        //sends a message
+
+//Prompt - I would like to ask my user a question
+
+
+        //requires a response
+
+//Confirm - I want to ask my user a YES or NO question
+
+        //sends a yes or no question
+
