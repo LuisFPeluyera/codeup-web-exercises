50e366fb93b1f87b0be3eb19bd2b1bce5e655be4
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-09-06 15:50:39 -0500

    jquery mouse events exercise completed

--- js/jQuery-js.js
+++ js/jQuery-js.js
@@ -31,3 +31,27 @@
 // $("li").css("font-size","20px");
 //
 // $("li,h1,p").css("font-weight","700");
+
+// *********************************MOUSE EVENTS EXERCISE
+
+
+
+let myH1= document.getElementById("myH1");
+// myH1.addEventListener("click", ()=>{alert("click")})
+
+$("h1").on("click",function () {
+    $(this).css("background-color", "black")
+});
+
+$("p").on("dblclick", function (){
+    $("p").css("font-size", "18px")
+})
+
+
+$("li").on({
+    mouseenter: function () {
+    $("li").css("color", "red");
+},
+    mouseleave: function () {
+        $("li").css("color", "black");
+    } })
