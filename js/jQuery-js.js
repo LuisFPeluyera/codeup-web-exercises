6f44cafa372ec71957cc8a9fda406bdd4e1396ee
luisfpeluyera <luis.f.peluyera@gmail.com>
2023-09-06 13:57:02 -0500

    jquery selectors exercise complete

--- js/jQuery-js.js
+++ js/jQuery-js.js
@@ -1,6 +1,33 @@
-$(function () {
-    // alert("Document has loaded!")
-});
+// $(function () {
+//     alert($("#myDiv").html())
+// });
+//
+//
+//
+// $("#myDiv").html("This is my new div!");
+//
+//
+//
+// console.log($("#mySpan").css());
 
 
-console.log($("p").html());
+// $(function () {
+//     alert($(".list-item").html());
+// });
+
+
+
+
+//**********************************PART TWO
+
+
+// $(".codeup").css("border", "1px solid black");
+
+
+
+//**********************************PART TRES
+
+
+// $("li").css("font-size","20px");
+//
+// $("li,h1,p").css("font-weight","700");
